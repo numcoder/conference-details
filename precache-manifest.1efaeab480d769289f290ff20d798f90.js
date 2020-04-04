59079e59d92f84ad19f62c76a7f9425af7b0b0f9
numcoder <lopscyclops@gmail.com>
2020-04-04 16:26:11 +0530

    Updates

---
+++ precache-manifest.1efaeab480d769289f290ff20d798f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363ab9e3bd42efbb88e6032a051b3fd0",
+    "url": "/conference-details/index.html"
+  },
+  {
+    "revision": "e6874c48b79dcc9c346b",
+    "url": "/conference-details/static/css/main.c5567afa.chunk.css"
+  },
+  {
+    "revision": "3433fc9d39d9c4c77cc8",
+    "url": "/conference-details/static/js/2.f11be445.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/conference-details/static/js/2.f11be445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6874c48b79dcc9c346b",
+    "url": "/conference-details/static/js/main.ff0a5f90.chunk.js"
+  },
+  {
+    "revision": "c3225a9e73cb47230dac",
+    "url": "/conference-details/static/js/runtime-main.28405ac2.js"
+  }
+]);
